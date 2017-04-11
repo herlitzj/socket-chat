398df7c13bda93eb2f3ca3f1e7d30ab5484dd40b
Ubuntu <ubuntu@ip-172-31-37-238.us-west-2.compute.internal>
2017-04-11 17:16:50 +0000

    moved chat templating to server. cleaned up templates and views.

--- chat.js
+++ chat.js
@@ -7,8 +7,23 @@ var handlebars = require('express-handlebars').create({
 
 var bodyParser = require('body-parser');
 var request = require('request');
-//var mysql = require('mysql');
-//var session = require('client-sessions');
+var fs = require('fs');
+var mysql = require('mysql');
+var session = require('client-sessions');
+
+app.use(session({
+    cookieName: 'session',
+    secret: 'cookie id',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+}));
+
+var mysql_connection = mysql.createConnection({
+    host: 'cs361.cdm64kabqtwv.us-west-2.rds.amazonaws.com',
+    user: 'wcamiller',
+    password: 'cs361projectb',
+    database: 'eridanus'
+})
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -24,12 +39,15 @@ app.set('port', 3007);
 app.use(express.static('public'));
 app.use(express.static(__dirname));
 
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-    res.sendFile('/index.html');
+    res.render('login');
+});
+
+app.get('/chat', function(req, res) {
+    res.render('chat');
 });
 
 io.on('connection', function(socket) {
@@ -40,11 +58,17 @@ io.on('connection', function(socket) {
 
     socket.on('chat message', function(msg) {
         var date = new Date();
-        var str = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-        io.emit('chat message', {
+        var time_str = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        var source = fs.readFileSync("./views/layouts/chat_template.handlebars");
+        var template = require("./views/layouts/chat_template.handlebars");
+        var msg_data = {
             username: 'username',
             msg: msg,
-            time: str
+            time: time_str
+        };
+        var html = template(msg_data);
+        io.emit('chat message', {
+            html: html
         });
     });
 });

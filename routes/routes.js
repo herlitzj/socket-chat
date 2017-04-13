1a73e3b69ed2de5cc56eb230503d6ea50ff6ca7c
Jeff Herlitz <jherlitz@nygenome.org>
2017-04-13 11:50:19 -0400

    simplied logout

--- routes/routes.js
+++ routes/routes.js
@@ -23,9 +23,7 @@ router.get('/login', function(req, res) {
 });
 
 router.get('/logout', function(req, res) {
-	if (req.session && req.session.user) {
-		req.session.user = null;
-	}
+	req.session.reset()
 	res.redirect("/users/login");
 })
 

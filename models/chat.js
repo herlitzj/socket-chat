5d6705c048bb9250b215fab2ca2ce8af41821f90
Jeff Herlitz <jherlitz@nygenome.org>
2017-05-24 17:00:53 -0400

    added ability to leave dm and add users

--- models/chat.js
+++ models/chat.js
@@ -60,6 +60,21 @@ class Chat {
         }
     })
   }
+  mark_inactive(user_id, chat_id, callback) {
+    var query_string = "DELETE FROM chat_participants WHERE user_id = ? AND chat_id = ?";
+    var values = [user_id, chat_id];
+    var query = db.build_query(query_string, values);
+
+    db.connection.query(query, (error, results, fields) => {
+        if (error) {
+            var err = new Error(error)
+            err.code = http_codes.NOT_FOUND;
+            return callback(err);
+        } else {
+            return callback(null);
+        }
+    })
+  }
   get_direct_messages(user_id, callback) {
     var query_string = "SELECT c.id, group_concat(u.username SEPARATOR ', ') name, count(u.username) + 1 participants\n" +
                         "FROM chats c\n" +
@@ -87,8 +102,8 @@ class Chat {
     })
   }
   create_direct_message(chat_participants, username, callback) {
-    if(!chat_participants.includes(username)) chat_participants = chat_participants + ',' + username;
-    var chatParticipants = chat_participants.split(',').map((cp) => cp.replace(' ', '')); // split into an array on commas and strip whitespace
+    if(!chat_participants.includes(username)) chat_participants = chat_participants + ', ' + username; //add current user if not already in string
+    var chatParticipantsArray = Chat.get_user_array_from_string(chat_participants);
 
     db.connection.beginTransaction(function(err) {
       if (err) { throw err; }
@@ -104,7 +119,7 @@ class Chat {
         var query_string = "INSERT INTO chat_participants (chat_id, user_id, created_at, updated_at)\n" +
                             "SELECT ?, id, NOW(), NOW()\n" +
                             "FROM users WHERE username in (?);"
-        var values = [results.insertId, chatParticipants];
+        var values = [results.insertId, chatParticipantsArray];
         var query = db.build_query(query_string, values);
 
         db.connection.query(query, function (error, results, fields) {
@@ -126,6 +141,26 @@ class Chat {
       });
     });
   }
+  add_users_to_dm(users, direct_message_id, callback) {
+    var userArray = Chat.get_user_array_from_string(users);
+    var query_string = "INSERT INTO chat_participants (chat_id, user_id, created_at, updated_at)\n" +
+                        "SELECT ?, id, NOW(), NOW()\n" +
+                        "FROM users WHERE username in (?);"
+    var values = [direct_message_id, userArray];
+    var query = db.build_query(query_string, values);
+
+    console.log(query);
+
+    db.connection.query(query, (error, results, fields) => {
+        if (error) {
+            var err = new Error(error)
+            err.code = http_codes.NOT_FOUND;
+            return callback(err);
+        } else {
+            return callback(null, direct_message_id);
+        }
+    })
+  }
   verify_direct_message(user_id, direct_message_id, callback) {
     var query_string = "SELECT id FROM chat_participants WHERE user_id = ? AND chat_id = ?"
     var values = [user_id, direct_message_id]
@@ -143,6 +178,7 @@ class Chat {
         }
     })
   }
+  static get_user_array_from_string(user_string) { return user_string.split(',').map(x => x.replace(' ', '')); } // split into an array on commas and strip whitespace
 }
 
 module.exports = Chat;
